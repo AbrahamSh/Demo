ee1404887054698c7d0eba03e588497dfb3da62d
Abraham Shapiro <a.shapirof@gmail.com>
2020-09-28 20:07:15 -0500

    Prueba2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Abraham Shapiro</Text>
+      <Text>No quiero que salga mi nombre</Text>
       <StatusBar style="auto" />
     </View>
   );
